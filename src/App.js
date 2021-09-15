55efe4fba9769b6c3f677eaf7efd4281384b7899
ANKUR <ankur02srma@gmail.com>
2021-09-15 14:32:28 +0530

    simple react pagination

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [postPerPage] = useState(5)
+
+  const url = 'https://jsonplaceholder.typicode.com/posts'
+
+  // get current post
+  const indexOfLastPost = currentPage * postPerPage
+  const indexOfFirstPost = indexOfLastPost - postPerPage
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost)
+
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      setLoading(true)
+      const res = await axios.get(url)
+      setPosts(res.data)
+      setLoading(false)
+    }
+    fetchPost()
+  }, [])
+
+  // console.log(posts);
+
+  return <div className="App">
+    <h1>Pagination Example</h1>
+    <Posts posts={currentPosts} loading={loading} />
+    <Pagination postsPerPage={postPerPage} totalPosts={posts.length} paginate={paginate} />
+  </div>;
 }
 
 export default App;
